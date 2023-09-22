5084387b41a34157c32c306606991711d70be805
JohannGourmelon <johann.gourmelon@oclock.school>
2023-09-22 15:44:07 +0200

    save before test first fix

--- src/Controller/API/TableController.php
+++ src/Controller/API/TableController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\API;
 
 use App\Repository\TableRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,8 +20,8 @@ class TableController extends AbstractController
         //  récupérer les tables
         $tables = $tableRepository->findAll();
 
-        // on retour les films en json
-        return $this->json($tables, Response::HTTP_OK);
+        // on retourne les tables en json
+        return $this->json($tables, Response::HTTP_OK, [], ["groups" => "tables"]);
     }
 
     /**
@@ -30,7 +32,28 @@ class TableController extends AbstractController
         //  récupérer les tables
         $tables = $tableRepository->findAllByUser($id);
 
-        // on retour les films en json
+        // on retourne les tables en json
         return $this->json($tables, Response::HTTP_OK, [], ["groups" => "tables"]);
+    }
+
+    /**
+     * @Route("/api/tables/{id}", name="app_api_table_show", methods={"PUT"})
+     */
+    public function edit($id, Request $request, TableRepository $tableRepository, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $table = $tableRepository->find($id);
+
+        if (!$table) {
+            return $this->json(['message' => 'Article non trouvé.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $table->setActive($data['active']);
+
+        $entityManager->flush();
+
+
+        return $this->json($table, Response::HTTP_OK,[], ["groups" => "tables"]);
     }
 }
