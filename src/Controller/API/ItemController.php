c3a8eed33ca0ab3c4a390e354cbe337c3a405f7d
Marc-LY <marc.ly@oclock.school>
2023-09-22 09:46:25 +0200

    Create ItemController Api

---
+++ src/Controller/API/ItemController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ItemController extends AbstractController
+{
+    /**
+     * @Route("/a/p/i/item", name="app_a_p_i_item")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/API/ItemController.php',
+        ]);
+    }
+}
