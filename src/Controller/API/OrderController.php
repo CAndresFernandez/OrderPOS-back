725ffca95d147ede012e6b7372c13ff513ff53bf
JohannGourmelon <johann.gourmelon@oclock.school>
2023-09-27 17:12:01 +0200

    orderController ok

--- src/Controller/API/OrderController.php
+++ src/Controller/API/OrderController.php
@@ -104,10 +104,14 @@ class OrderController extends AbstractController
     {
         $data = json_decode($request->getContent(), true);
         try {
-            $order = $serializer->deserialize($request->getContent(), Order::class, 'json',
+            $order = $serializer->deserialize(
+                $request->getContent(),
+                Order::class,
+                'json',
                 [
                     AbstractNormalizer::IGNORED_ATTRIBUTES => ['user_id', 'relatedTable_id'],
-                ]);
+                ]
+            );
 
             $table = $this->em->getRepository(Table::class)->find($data['relatedTable_id']);
             $user = $this->em->getRepository(User::class)->find($data['user_id']);
@@ -150,31 +154,119 @@ class OrderController extends AbstractController
      */
     public function addItemToOrder(int $id, SerializerInterface $serializer, ValidatorInterface $validator, OrderRepository $orderRepository, Request $request): JsonResponse
     {
+        //je récupère la commande en cours
         $order = $orderRepository->find($id);
-
+        //je vérifie que la commande en cours existe bien
         if (!$order) {
             return $this->json(['message' => 'Commande non trouvée.'], Response::HTTP_NOT_FOUND);
         } elseif ($order->getStatus() > 1) {
             return $this->json(['message' => 'Commande déjà envoyée.'], Response::HTTP_FORBIDDEN);
         }
 
-        $data = json_decode($request->getContent(), true);
-        $newOrderItems = [];
+        // je récupère la collection des orderItems de la commande actuelle (peut être vide si nouvelle commande par ex)
+        $oldOrderItems = $order->getOrderItems()->getValues();
+        //cette boucle sert à harmoniser la collection en cas de données rajouter en brut dans la bdd
+        //cette boucle ne devrait normalement pas servir dans l'usage normal de l'application
+        dump("collection d'orderItems de order initiale avant factorisation", $oldOrderItems);
+        $orderItemsToDelete = [];
+        foreach ($oldOrderItems as $key => $oldOrderItem) {
+            // pour chaque orderItem je récupère son id, sa quantité, son commentaire et l'id de l'item correspondant
+            $itemId = $oldOrderItem->getItem()->getId();
+            $id = $oldOrderItem->getId();
+            $comment = $oldOrderItem->getComment();
+            $quantity = $oldOrderItem->getQuantity();
+            $orderItemKeyToDelete = [];
+            foreach ($oldOrderItems as $keyB => $oldOrderItemB) {
+                //pour chaque orderItem je cherche si un autre orderItem relié au même item et ayant le même commentaire existe
+                //si c'est le cas j'ajoute les quantités du second au premier puis je viens supprimer le second de la collection
+                //d'orderItems
+                if (($itemId == $oldOrderItemB->getItem()->getId()) && ($id != $oldOrderItemB->getId()) && ($comment == $oldOrderItemB->getComment())) {
+                    $quantity += $oldOrderItemB->getQuantity();
+                    $orderItemKeyToDelete[] = $keyB;
+                    $orderItemsToDelete[] = $oldOrderItemB;
+                    // dump($key, 'reste au profit de', $keyB, $itemId, $oldOrderItemB->getItem()->getId());
+                }
+            }
+            foreach ($orderItemKeyToDelete as $keyToDelete) {
+                unset($oldOrderItems[$keyToDelete]);
+            }
+            $oldOrderItem->setQuantity($quantity);
+        }
+        // dump($oldOrderItems, $order->getOrderItems()->getValues());
+        //je supprime les orderItems qui ont été factorisés
+        foreach ($orderItemsToDelete as $orderItem) {
+            $this->em->getRepository(OrderItem::class)->remove($orderItem);
+            $order->removeOrderItem($orderItem);
+        }
+        // dump($oldOrderItems, $order->getOrderItems()->getValues());
+        //je vérifie que les opérations précédentes
 
+
+
+
+
+        // je récupère la liste des orderItems de la requête
+        $data = json_decode($request->getContent(), true);
+        //je créé un tableau vide pour acceuillir la liste actualisée des orderItems
+        $newOrderItems = [];
         foreach ($data as $orderItem) {
             $newItem = json_encode($orderItem);
             $newOrderItem = $serializer->deserialize($newItem, OrderItem::class, 'json', [
                 AbstractNormalizer::IGNORED_ATTRIBUTES => ['item'],
             ]);
             $item = $this->em->getRepository(Item::class)->find($orderItem['item']);
-            $newOrderItem->setRelatedOrder($order);
             $newOrderItem->setItem($item);
+            //La liste obtenue contient des orderItems sans Id et sans orderId
             $newOrderItems[] = $newOrderItem;
         }
-
+        dump("collection d'orderItems de order", $order->getOrderItems()->getValues(), "oldOrderItems virtuel", $oldOrderItems, "newOrderItems virtuel", $newOrderItems);
+        // dd($oldOrderItems, $newOrderItems);
+        // je créé une nouvelle liste vide d'orderItems mise à jour
+        $updatedOrderItems = [];
+        //je compare l'ancienne et la nouvelle liste d'orderItems 
+        //pour remplir $updatedOrderItems
+        $oldOrderItemToSave = []; //je créé un tableau avec les clés des orderItems existants qui seront conservés
         foreach ($newOrderItems as $newOrderItem) {
-            $order->addOrderItem($newOrderItem);
+            $itemId = $newOrderItem->getItem()->getId();
+            $comment = $newOrderItem->getComment();
+            $quantity = $newOrderItem->getQuantity();
+            $newOrderItemToConserve = true;
+            foreach ($oldOrderItems as $key => $oldOrderItem) {
+                //pour chaque newOrderItem je cherche si un oldOrderItem relié au même item et ayant le même commentaire existe
+                //si c'est le cas je viens actualiser l'ancien avec les valeurs du nouveau puis je l'ajoute à la nouvelle liste MAJ
+                if (($itemId == $oldOrderItem->getItem()->getId()) && ($comment == $oldOrderItem->getComment())) {
+                    //
+                    //
+                    // ICI JE PEUX RAJOUTER UN FILTRE ISSENT
+                    //
+                    //
+                    $oldOrderItem->setQuantity($quantity); //j'actualise la quantité
+                    $oldOrderItemToSave[] = $key;
+                    $updatedOrderItems[] = $oldOrderItem;
+                    $newOrderItemToConserve = false; //si je trouve un ancien order item correspondant alors je ne conserve pas le nouveau
+                }
+            }
+            if ($newOrderItemToConserve) {
+                $updatedOrderItems[] = $newOrderItem;
+            }
         }
+        dump("updatedOrderItems", $updatedOrderItems);
+        //je supprime les orderItems conservés de $oldOrderItems
+        foreach ($oldOrderItemToSave as $keyToDelete) {
+            unset($oldOrderItems[$keyToDelete]);
+        }
+        dump("orderItemsToSave", $oldOrderItemToSave, "oldOrderItems", $oldOrderItems);
+        //je supprime les oldOrderItems n'étant pas conservés
+        foreach ($oldOrderItems as $oldOrderItem) {
+            $this->em->getRepository(OrderItem::class)->remove($oldOrderItem);
+            $order->removeOrderItem($oldOrderItem);
+        }
+        $this->em->persist($order);
+        foreach ($updatedOrderItems as $updatedOrderItem) {
+            $order->addOrderItem($updatedOrderItem);
+        }
+        
+        // dd($order);
 
         // je check si mon order contient des erreurs
         $errors = $validator->validate($order);
@@ -200,7 +292,7 @@ class OrderController extends AbstractController
     }
 
     /**
-     * @Route("/api/orders/{id}", name="app_api_order_modifyStatus", methods={"PUT"})
+     * @Route("/api/orders/{id}/status", name="app_api_order_modifyStatus", methods={"PUT"})
      */
     public function modifyStatus(int $id, OrderRepository $orderRepository)
     {
@@ -212,6 +304,4 @@ class OrderController extends AbstractController
             return $this->json(['message' => 'Commande déjà envoyée.'], Response::HTTP_FORBIDDEN, ["Location" => $this->generateUrl("app_api_order_list")], ["groups" => "orders"]);
         }
     }
-
-    
 }
