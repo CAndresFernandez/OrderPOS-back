7399b1327b7819936e835050d64d3076830e3994
Andres Fernandez <andres-carlos.fernandez-baker@oclock.school>
2023-09-28 17:04:07 +0200

    fixed link buttons on index template, configured the form

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,17 +4,32 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('menu_position')
-            ->add('active')
+            ->add('name', TextType::class, [
+                'constraints' => new NotBlank(),
+                'attr' => [
+                    'placeholder' => 'ex. Pizza, Beer, Desserts',
+                ],
+            ])
+            ->add('menu_position', IntegerType::class, [
+                'constraints' => new NotBlank(),
+                'label' => 'Display position on the menu.',
+            ])
+            ->add('active', CheckboxType::class, [
+                'label' => 'Is this an active menu category?',
+                'required' => true,
+            ])
         ;
     }
 
