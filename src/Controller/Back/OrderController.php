1c921fc07f6969cad96a812e2b974e3cbb5e1aa5
Marc-LY <marc.ly@oclock.school>
2023-10-11 11:22:51 +0200

    Done - Add function to cancel an order in APIController and UPDATE view in BO

--- src/Controller/Back/OrderController.php
+++ src/Controller/Back/OrderController.php
@@ -2,11 +2,8 @@
 
 namespace App\Controller\Back;
 
-use App\Entity\Order;
-use App\Form\OrderType;
 use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,66 +22,4 @@ class OrderController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/new", name="app_back_order_new", methods={"GET", "POST"})
-     */
-    // public function new(Request $request, OrderRepository $orderRepository): Response
-    // {
-    //     $order = new Order();
-    //     $form = $this->createForm(OrderType::class, $order);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $orderRepository->add($order, true);
-
-    //         return $this->redirectToRoute('app_back_order_list', [], Response::HTTP_SEE_OTHER);
-    //     }
-
-    //     return $this->renderForm('back/order/new.html.twig', [
-    //         'order' => $order,
-    //         'form' => $form,
-    //     ]);
-    // }
-
-    /**
-     * @Route("/{id}", name="app_back_order_show", methods={"GET"})
-     */
-    public function show(Order $order): Response
-    {
-        return $this->render('back/order/show.html.twig', [
-            'order' => $order,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="app_back_order_edit", methods={"GET", "POST"})
-     */
-    public function edit(Request $request, Order $order, OrderRepository $orderRepository): Response
-    {
-        $form = $this->createForm(OrderType::class, $order);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $orderRepository->add($order, true);
-
-            return $this->redirectToRoute('app_back_order_list', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('back/order/edit.html.twig', [
-            'order' => $order,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="app_back_order_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Order $order, OrderRepository $orderRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$order->getId(), $request->request->get('_token'))) {
-            $orderRepository->remove($order, true);
-        }
-
-        return $this->redirectToRoute('app_back_order_list', [], Response::HTTP_SEE_OTHER);
-    }
 }
